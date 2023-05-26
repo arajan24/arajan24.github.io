aa48a9ad721ed85cd91c9d06d0f1cddb8fed2ce6
arajan24 <47431175+arajan24@users.noreply.github.com>
2023-05-25 20:40:47 -0500

    Create password.js

---
+++ assets/js/password.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{
+        
+	$("#submit").click(function()  // If submit button is click.
+	{
+	      if ($("input#pwd").val() == "password") // Input button with id="pwd" 
+              {                                       // Choose password and replace it.
+	        $(".disabled").attr('controls',true); //  You disabled the controls.
+	        $(".error").text("nice!");
+	          
+	      }else    
+	      $(".error").text("wrong password, ask the admin or try again.");
+	});
+
+ });
