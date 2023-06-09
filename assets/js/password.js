f4d2cf895fa6abbf0b3d57ae8f5af252f2ea6a94
Adithya Rajan <adithyarajan24@gmail.com>
2023-06-08 20:44:21 -0500

    edit

--- assets/js/password.js
+++ assets/js/password.js
@@ -3,7 +3,7 @@ $(document).ready(function()
         
 	$("#submit").click(function()  // If submit button is click.
 	{
-	      if ($("input#pwd").val() == "password1") // Input button with id="pwd" 
+	      if ($("input#pwd").val() == "password") // Input button with id="pwd" 
               {                                       // Choose password and replace it.
 	        $(".disabled").attr('controls',true); //  You disabled the controls.
 	        $(".error").text("nice!");
